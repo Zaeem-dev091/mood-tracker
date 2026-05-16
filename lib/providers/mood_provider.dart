36b32cb8f10c831a6673ec9fa789393c14f15e32
Muhammad Zaeem <nadeemzaim091@gmail.com>
2026-05-16 21:31:03 +0500

    Update mood provider

--- lib/providers/mood_provider.dart
+++ lib/providers/mood_provider.dart
@@ -3,19 +3,10 @@ import '../models/mood_entry.dart';
 
 class MoodProvider extends ChangeNotifier {
   final List<MoodEntry> _entries = [];
-  Mood? _selectedMood;
-
-  List<MoodEntry> get entries => List.unmodifiable(_entries);
 
   List<MoodEntry> get recentEntries {
     final sorted = [..._entries]..sort((a, b) => b.date.compareTo(a.date));
     return sorted.take(7).toList();
-  }
-
-  Mood? get selectedMood => _selectedMood;
-
-  void selectMood(Mood mood) {
-    _selectedMood = mood;
   }
 
   void logMood(Mood mood) {
@@ -25,12 +16,6 @@ class MoodProvider extends ChangeNotifier {
       date: DateTime.now(),
     );
     _entries.add(entry);
-    _selectedMood = null;
-    notifyListeners();
-  }
-
-  void clearSelection() {
-    _selectedMood = null;
     notifyListeners();
   }
 }
